45da2b2f1891e1a7de4d87a91aad69aa6961b182
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-26 00:30:05 +0100

    Routes

---
+++ src/Routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+
+const Routes = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default Routes
