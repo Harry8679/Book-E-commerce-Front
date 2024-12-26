ed027c32db9ec872a80920be11eb46c3e0a3bf6c
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-26 11:32:56 +0100

    Home Page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/signin' element={<Signin />} />
         <Route path='/signup' element={<Signup />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
