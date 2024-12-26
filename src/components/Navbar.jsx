9a6d0114f91a6cabde1a2ca68ccb5068b2ff9fd0
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-26 12:45:09 +0100

    Links added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -8,38 +9,57 @@ const Navbar = () => {
           {/* Left Section */}
           <div className="flex items-center space-x-6">
             <div className="text-2xl font-bold">
-              <a href="/" className="text-teal-400">
+              <NavLink
+                to="/"
+                className="text-teal-400"
+              >
                 Mon Logo
-              </a>
+              </NavLink>
             </div>
-            <a
-              href="#accueil"
-              className="hover:text-teal-400 transition duration-300"
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-teal-400 border-b-2 border-teal-400"
+                  : "hover:text-teal-400 transition duration-300"
+              }
             >
               Accueil
-            </a>
-            <a
-              href="#listes-des-livres"
-              className="hover:text-teal-400 transition duration-300"
+            </NavLink>
+            <NavLink
+              to="/listes-des-livres"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-teal-400 border-b-2 border-teal-400"
+                  : "hover:text-teal-400 transition duration-300"
+              }
             >
               Liste des livres
-            </a>
+            </NavLink>
           </div>
 
           {/* Right Section */}
           <div className="flex items-center space-x-4">
-            <a
-              href="#inscription"
-              className="px-4 py-2 bg-teal-500 rounded hover:bg-teal-600 transition duration-300"
+            <NavLink
+              to="/signup"
+              className={({ isActive }) =>
+                isActive
+                  ? "px-4 py-2 bg-teal-600 rounded"
+                  : "px-4 py-2 bg-teal-500 rounded hover:bg-teal-600 transition duration-300"
+              }
             >
               Inscription
-            </a>
-            <a
-              href="#connexion"
-              className="px-4 py-2 border border-teal-500 rounded hover:bg-teal-500 hover:text-white transition duration-300"
+            </NavLink>
+            <NavLink
+              to="/signin"
+              className={({ isActive }) =>
+                isActive
+                  ? "px-4 py-2 border border-teal-500 bg-teal-500 text-white rounded"
+                  : "px-4 py-2 border border-teal-500 rounded hover:bg-teal-500 hover:text-white transition duration-300"
+              }
             >
               Connexion
-            </a>
+            </NavLink>
           </div>
         </div>
       </div>
