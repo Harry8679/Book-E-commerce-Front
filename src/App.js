dd8ed05e0c9236225bf2c4950360b91eb9015bff
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-26 12:34:36 +0100

    Navbar added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path='/signin' element={<Signin />} />
         <Route path='/signup' element={<Signup />} />
