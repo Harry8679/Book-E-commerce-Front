ad68956422c9044eafa65ba2d140a9d2e925e721
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-26 23:58:23 +0100

    Signup Form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Layout from '../components/Layout'
 
 const Signup = () => {
+  const signUpForm = () => {
+    <form action="">
+      
+    </form>
+  };
   return (
     <Layout title='Sign Up Page' description='Node React E-commerce App'>Hello : {process.env.REACT_APP_API_URL}</Layout>
   )
