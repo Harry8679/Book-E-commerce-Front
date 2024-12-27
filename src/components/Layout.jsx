dd45baeb9f4297fb7c08446b0c891608e084432d
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-27 11:37:27 +0100

    Useless comment

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,9 +8,6 @@ const Layout = ({ title, description, className, children }) => {
         <div class="container mx-auto">
           <h1 class="text-4xl font-bold">{title}</h1>
           <p class="mt-4 text-lg">{description}</p>
-          {/* <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            En savoir plus
-          </button> */}
         </div>
       </section>
       <div className={className}>{children}</div>
