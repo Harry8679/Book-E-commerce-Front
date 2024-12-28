e470212328b022a2c2e210f1d3bdadf42e6bc96b
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-28 09:52:10 +0100

    Layout updated

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Layout = ({ title, description, className, children }) => {
   return (
     <div>
-      <section className="bg-gray-200 text-gray-800 py-10">
+      <section className="bg-gray-200 text-gray-800 py-10 mx-auto text-center">
+        {/* <div class="container mx-auto text-center"> */}
         <div className="container mx-auto">
           <h1 className="text-4xl font-bold">{title}</h1>
           <p className="mt-4 text-lg">{description}</p>
