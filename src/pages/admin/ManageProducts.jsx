a8df0a6e497edc7e48ab42f2bff8d506e6f5896f
Harry MacCode <emarh.harry.code@gmail.com>
2025-01-14 10:30:39 +0100

    Manage Products

---
+++ src/pages/admin/ManageProducts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ManageProducts = () => {
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-2xl font-bold text-teal-600 mb-4">Gestion des Produits</h1>
+      <p>Gestion des produits ici...</p>
+    </div>
+  );
+};
+
+export default ManageProducts;
