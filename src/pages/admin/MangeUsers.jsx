f2a3b41c55f239abfbc87d0135f4024fe1cd4cc0
Harry MacCode <emarh.harry.code@gmail.com>
2025-01-14 10:29:15 +0100

    Manage Users

---
+++ src/pages/admin/MangeUsers.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ManageUsers = () => {
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-2xl font-bold text-teal-600 mb-4">Gestion des Utilisateurs</h1>
+      <p>Liste des utilisateurs à gérer ici...</p>
+    </div>
+  );
+};
+
+export default ManageUsers;
