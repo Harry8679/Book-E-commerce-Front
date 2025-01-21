668169df0167a3051902f26d9f7b359d97f69f9a
Harry MacCode <emarh.harry.code@gmail.com>
2025-01-21 02:14:26 +0100

    Image updated

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,11 +1,14 @@
 const Book = ({ product }) => {
   return (
     <div className="bg-white shadow-md rounded-lg overflow-hidden">
-      <img
-        src={product.imageUrl} // Utilisation du champ `imageUrl`
-        alt={product.name}
-        className="w-full h-48 object-cover"
-      />
+      {/* Conteneur pour contrôler le placement et la taille */}
+      <div className="w-full h-48 relative">
+        <img
+          src={product.imageUrl} // Utilisation du champ `imageUrl`
+          alt={product.name}
+          className="w-full h-full object-contain" // Utilisation de `object-contain` pour éviter les coupes
+        />
+      </div>
       <div className="p-4">
         <h2 className="text-lg font-bold text-gray-800">{product.name}</h2>
         <p className="text-gray-600">{product.description.substring(0, 25)}...</p>
