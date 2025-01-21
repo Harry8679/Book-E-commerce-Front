a76931604a009fa725e75c2ac3dc212779c30865
Harry MacCode <emarh.harry.code@gmail.com>
2025-01-21 12:41:22 +0100

    App.js updated

--- src/App.js
+++ src/App.js
@@ -23,13 +23,16 @@ import ViewCategory from './pages/admin/ViewCategory';
 import EditProduct from './pages/admin/EditProduct';
 import ViewProduct from './pages/admin/ViewProduct';
 import CreateProduct from './pages/admin/CreateProduct';
-import ProductDetails from './pages/ProductDetails'; // Nouvelle page pour les détails des produits
-import Cart from './pages/Cart'; // Nouvelle page pour le panier
+import ProductDetails from './pages/ProductDetails';
+import Cart from './pages/Cart';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [userRole, setUserRole] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  // 🛒 Gestion du panier
+  const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -47,6 +50,29 @@ function App() {
     setLoading(false);
   }, []);
 
+  // Fonction pour ajouter un produit au panier
+  const addToCart = (product) => {
+    setCartItems((prevItems) => {
+      const isProductInCart = prevItems.find((item) => item._id === product._id);
+
+      if (isProductInCart) {
+        // Augmentez la quantité si le produit est déjà dans le panier
+        return prevItems.map((item) =>
+          item._id === product._id ? { ...item, quantity: item.quantity + 1 } : item
+        );
+      } else {
+        // Ajoutez un nouveau produit au panier
+        return [...prevItems, { ...product, quantity: 1 }];
+      }
+    });
+  };
+
+  // Fonction pour supprimer un produit du panier
+  const removeFromCart = (productId) => {
+    setCartItems((prevItems) => prevItems.filter((item) => item._id !== productId));
+  };
+
+  // Fonction pour se connecter
   const login = (user, token) => {
     if (user && token) {
       localStorage.setItem('token', token);
@@ -58,10 +84,12 @@ function App() {
     }
   };
 
+  // Fonction pour se déconnecter
   const logout = () => {
     localStorage.clear();
     setIsAuthenticated(false);
     setUserRole(null);
+    setCartItems([]); // Vide le panier lors de la déconnexion
   };
 
   if (loading) {
@@ -70,33 +98,138 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Navbar isAuthenticated={isAuthenticated} logout={logout} />
+      <Navbar isAuthenticated={isAuthenticated} logout={logout} cartItems={cartItems} />
       <Routes>
-        <Route path="/signin" element={isAuthenticated ? (<Navigate to="/" />) : (<Signin login={login} />)} />
-        <Route path="/signup" element={isAuthenticated ? (<Navigate to="/" />) : (<Signup />)} />
-        <Route path="/dashboard" element={isAuthenticated ? (<Dashboard />) : (<Navigate to="/signin" />)} />
-        <Route path="/profile" element={isAuthenticated ? (<Profile />) : (<Navigate to="/signin" />)} />
-        <Route path="/update-profile" element={isAuthenticated ? (<UpdateProfile />) : (<Navigate to="/signin" />)} />
-        <Route path="/update-password" element={isAuthenticated ? (<UpdatePassword />) : (<Navigate to="/signin" />)} />
-        <Route path="/listes-des-livres" element={<Books />} />
+        {/* Routes publiques */}
+        <Route
+          path="/signin"
+          element={isAuthenticated ? <Navigate to="/" /> : <Signin login={login} />}
+        />
+        <Route
+          path="/signup"
+          element={isAuthenticated ? <Navigate to="/" /> : <Signup />}
+        />
+        <Route
+          path="/dashboard"
+          element={isAuthenticated ? <Dashboard /> : <Navigate to="/signin" />}
+        />
+        <Route
+          path="/profile"
+          element={isAuthenticated ? <Profile /> : <Navigate to="/signin" />}
+        />
+        <Route
+          path="/update-profile"
+          element={isAuthenticated ? <UpdateProfile /> : <Navigate to="/signin" />}
+        />
+        <Route
+          path="/update-password"
+          element={isAuthenticated ? <UpdatePassword /> : <Navigate to="/signin" />}
+        />
+        <Route path="/listes-des-livres" element={<Books addToCart={addToCart} />} />
         <Route path="/" element={<Home />} />
-        <Route path="/books/:productId" element={<ProductDetails />} /> {/* Route pour les détails des produits */}
-        <Route path="/cart" element={<Cart />} /> {/* Route pour le panier */}
+        <Route path="/books/:productId" element={<ProductDetails />} />
+        <Route
+          path="/cart"
+          element={<Cart cartItems={cartItems} removeFromCart={removeFromCart} />}
+        />
 
-        <Route path="/admin/dashboard" element={<AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}><AdminDashboard /></AdminRoute>} />
-        <Route path="/admin/users" element={<AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}><ManageUsers /></AdminRoute>} />
-        <Route path="/admin/users/edit/:userId" element={<AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}><EditUser /></AdminRoute>} />
-        <Route path="/admin/users/view/:userId" element={<AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}><ViewUser /></AdminRoute>} />
-
-        <Route path="/admin/categories" element={<AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}><ManageCategories /></AdminRoute>} />
-        <Route path="/admin/categories/create" element={<AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}><CreateCategory /></AdminRoute>} />
-        <Route path="/admin/categories/edit/:categoryId" element={<AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}><EditCategory /></AdminRoute>} />
-        <Route path="/admin/categories/view/:categoryId" element={<AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}><ViewCategory /></AdminRoute>} />
-
-        <Route path="/admin/products" element={<AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}><ManageProducts /></AdminRoute>} />
-        <Route path="/admin/products/create" element={<AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}><CreateProduct /></AdminRoute>} />
-        <Route path="/admin/products/edit/:productId" element={<AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}><EditProduct /></AdminRoute>} />
-        <Route path="/admin/products/view/:productId" element={<AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}><ViewProduct /></AdminRoute>} />
+        {/* Routes Admin */}
+        <Route
+          path="/admin/dashboard"
+          element={
+            <AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}>
+              <AdminDashboard />
+            </AdminRoute>
+          }
+        />
+        <Route
+          path="/admin/users"
+          element={
+            <AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}>
+              <ManageUsers />
+            </AdminRoute>
+          }
+        />
+        <Route
+          path="/admin/users/edit/:userId"
+          element={
+            <AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}>
+              <EditUser />
+            </AdminRoute>
+          }
+        />
+        <Route
+          path="/admin/users/view/:userId"
+          element={
+            <AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}>
+              <ViewUser />
+            </AdminRoute>
+          }
+        />
+        <Route
+          path="/admin/categories"
+          element={
+            <AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}>
+              <ManageCategories />
+            </AdminRoute>
+          }
+        />
+        <Route
+          path="/admin/categories/create"
+          element={
+            <AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}>
+              <CreateCategory />
+            </AdminRoute>
+          }
+        />
+        <Route
+          path="/admin/categories/edit/:categoryId"
+          element={
+            <AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}>
+              <EditCategory />
+            </AdminRoute>
+          }
+        />
+        <Route
+          path="/admin/categories/view/:categoryId"
+          element={
+            <AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}>
+              <ViewCategory />
+            </AdminRoute>
+          }
+        />
+        <Route
+          path="/admin/products"
+          element={
+            <AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}>
+              <ManageProducts />
+            </AdminRoute>
+          }
+        />
+        <Route
+          path="/admin/products/create"
+          element={
+            <AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}>
+              <CreateProduct />
+            </AdminRoute>
+          }
+        />
+        <Route
+          path="/admin/products/edit/:productId"
+          element={
+            <AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}>
+              <EditProduct />
+            </AdminRoute>
+          }
+        />
+        <Route
+          path="/admin/products/view/:productId"
+          element={
+            <AdminRoute isAuthenticated={isAuthenticated} userRole={userRole}>
+              <ViewProduct />
+            </AdminRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
