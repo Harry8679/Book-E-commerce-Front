1dcf41b4c41fedf9a3983d9da7886e01f12661da
Harry MacCode <emarh.harry.code@gmail.com>
2025-01-23 23:45:17 +0100

    Stripe Checkout style

--- src/pages/StripeCheckout.jsx
+++ src/pages/StripeCheckout.jsx
@@ -3,13 +3,16 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import CheckoutForm from './CheckoutForm';
 
-const stripePromise = loadStripe('pk_test_51QkYSB2RfwNkGwDfMDbWJsQmS5BcIGHypKTNtcCJ9rlbWd3RqZfiCU0wy9nTLVNYOOjaYpB5EjIjySSva6t1EYXx00spFGqQDZ'); // Utilisez votre clé publique
+const stripePromise = loadStripe('pk_test_51QkYSB2RfwNkGwDfMDbWJsQmS5BcIGHypKTNtcCJ9rlbWd3RqZfiCU0wy9nTLVNYOOjaYpB5EjIjySSva6t1EYXx00spFGqQDZ'); // Votre clé publique Stripe
 
 const StripeCheckout = ({ amount }) => {
   return (
-    <Elements stripe={stripePromise}>
-      <CheckoutForm amount={amount} />
-    </Elements>
+    <div className="container mx-auto py-8">
+      <h1 className="text-3xl font-bold text-center mb-6">Paiement par Carte Bancaire</h1>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm amount={amount} />
+      </Elements>
+    </div>
   );
 };
 
