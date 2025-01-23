67dd90d8e8c7031b1cd6596adbe73418503fcc5c
Harry MacCode <emarh.harry.code@gmail.com>
2025-01-24 00:26:40 +0100

    Get the State

--- src/pages/StripeCheckout.jsx
+++ src/pages/StripeCheckout.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import CheckoutForm from './CheckoutForm';
 
 const stripePromise = loadStripe('pk_test_51QkYSB2RfwNkGwDfMDbWJsQmS5BcIGHypKTNtcCJ9rlbWd3RqZfiCU0wy9nTLVNYOOjaYpB5EjIjySSva6t1EYXx00spFGqQDZ'); // Votre clé publique Stripe
 
-const StripeCheckout = ({ amount }) => {
+const StripeCheckout = () => {
+  const { state } = useLocation(); // Récupérer le state contenant `amount`
+
+  if (!state?.amount) {
+    return <p>Montant invalide ou non spécifié.</p>;
+  }
+
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-3xl font-bold text-center mb-6">Paiement par Carte Bancaire</h1>
-      <Elements stripe={stripePromise}>
-        <CheckoutForm amount={amount} />
-      </Elements>
-    </div>
+    <Elements stripe={stripePromise}>
+      <CheckoutForm amount={state.amount} />
+    </Elements>
   );
 };
 
